001235d1d98b92f70d22eeb6f18075de6241cded
K4ds0n-F1l1pp1 <kadsonfilippi2007@gmail.com>
2025-01-02 09:58:05 -0300

    Tabelas

--- Cadastro-Empresa/src/veiculos.php
+++ Cadastro-Empresa/src/veiculos.php
@@ -1,14 +1,13 @@
 <?php
 
 $dsn = 'sqlite:veiculos.db';
-$dsnn = 'sqlite:motoristas.db';
 
 try {
-    $pdo = new PDO($dsn);
+    $pdoVeiculos = new PDO($dsn);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $pdo->exec("CREATE TABLE IF NOT EXISTS veiculos(
-    placa INTEGER PRIMARY KEY AUTOINCREMENT,
+    placa CHAR(7) NOT NULL,
     renavam CHAR(30) DEFAULT NULL,
     modelo CHAR(20) NOT NULL,
     marca CHAR(20) NOT NULL,
@@ -22,25 +21,5 @@ try {
 
 function listVeiculos($pdo) {
     $statement = $pdo->query("SELECT * FROM veiculos");
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
-}
-
-// Cria os motoristas:
-try {
-    $pdo = new PDO($dsnn);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $pdo->exec("CREATE TABLE IF NOT EXISTS motoristas(
-    CPF INTEGER PRIMARY KEY CHAR(11),
-    nome CHAR(200) NOT NULL,
-    rg CHAR(20) UNIQUE NOT NULL,
-    telefone CHAR(20) NOT NULL)");
-} catch (PDOException $e) {
-    die ("Erro ao procurar banco de dados.". $e->getMessage());
-}
-
-// Listar os motoristas:
-function listMotoristas($pdo) {
-    $statement = $pdo->query("SELECT * FROM motoristas");
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $statement ? $statement->fetchAll(PDO::FETCH_ASSOC) : [];
 }
