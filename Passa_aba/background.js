cebc138fbe771b04c406b81615214a99984350db
K4ds0n-F1l1pp1 <kadsonfilippi2007@gmail.com>
2025-01-10 09:21:22 -0300

    Passa aba

--- Passa_aba/background.js
+++ Passa_aba/background.js
@@ -1,12 +1,33 @@
-let currentTabIndex = 0;
+const intervalInMinutes = 3;
 
 function switchTab() {
   chrome.tabs.query({}, (tabs) => {
     if (tabs.length > 0) {
-      currentTabIndex = (currentTabIndex + 1) % tabs.length;
-      chrome.tabs.update(tabs[currentTabIndex].id, { active: true });
+
+      chrome.storage.local.get(["currentTabIndex"], (result) => {
+        let currentTabIndex = result.currentTabIndex || 0;
+
+        currentTabIndex = (currentTabIndex + 1) % tabs.length;
+        chrome.tabs.update(tabs[currentTabIndex].id, { active: true });
+
+        chrome.storage.local.set({ currentTabIndex });
+      });
     }
   });
 }
 
-setInterval(switchTab, 20000);
+chrome.runtime.onInstalled.addListener(() => {
+
+  chrome.alarms.create("tabSwitcher", { periodInMinutes: intervalInMinutes });
+  chrome.storage.local.set({ currentTabIndex: 0 }); // Inicializar o índice
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "tabSwitcher") {
+    switchTab();
+  }
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  chrome.alarms.create("tabSwitcher", { periodInMinutes: intervalInMinutes });
+});
